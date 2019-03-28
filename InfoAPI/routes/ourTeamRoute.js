f0ba98715b0b1daff7068a3475ae34c194cd211b
Keith Prevost <rickyprev@yahoo.com>
2019-03-28 19:31:42 -0400

    added all members according to website. Need links for personal web, linkedIn, and Github still

--- InfoAPI/routes/ourTeamRoute.js
+++ InfoAPI/routes/ourTeamRoute.js
@@ -3,7 +3,71 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/ourTeam", function(req, res, next) {
-  res.send({ name: "Express" });
+  res.send(
+    {
+      fname: "Gonzalo",
+      lname: "Reyes",
+      founderStatus: "Co-Founder",
+      position: "President",
+      personalWeb: "...",
+      github: "...",
+      linkedIn: "..."
+    },
+    {
+      fname: "Gaurav",
+      lname: "Jayasawal",
+      founderStatus: "Co-Founder",
+      position: "Vice President",
+      personalWeb: "...",
+      github: "...",
+      linkedIn: "..."
+    },
+    {
+      fname: "Keenan",
+      lname: "Kunzelman",
+      founderStatus: "Co-Founder",
+      position: "PR Chair",
+      personalWeb: "...",
+      github: "...",
+      linkedIn: "..."
+    },
+    {
+      fname: "Hung",
+      lname: "Nguyen",
+      founderStatus: "Co-Founder",
+      position: "Treasurer",
+      personalWeb: "...",
+      github: "...",
+      linkedIn: "..."
+    },
+    {
+      fname: "Keith",
+      lname: "Prevost",
+      founderStatus: "Co-Founder",
+      position: "PR Chair",
+      personalWeb: "...",
+      github: "...",
+      linkedIn: "..."
+    },
+    {
+      fname: "Khanh",
+      lname: "Le",
+      founderStatus: "Co-Founder",
+      position: "Secretary",
+      personalWeb: "...",
+      github: "...",
+      linkedIn: "..."
+    },
+    {
+      fname: "Michael",
+      lname: "Gates",
+      founderStatus: "Co-Founder",
+      position: "Avid Contributor",
+      personalWeb: "...",
+      github: "...",
+      linkedIn: "..."
+    }
+  );
 });
 
 module.exports = router;
